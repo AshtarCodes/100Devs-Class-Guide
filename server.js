f6bc8b0badea848814852e735a671dd84a631a31
Mario De Los Santos <mario.jdls@gmail.com>
2021-03-25 10:17:32 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const cors = require('cors')
+const { request, response } = require('express')
+const app = express()
+const PORT = 9000
+app.use(cors())
+
+let lessons = {
+    "week_01": {
+        "name": "Week_01",
+        "tagline": "Come learn 'How to Learn' and the basics of HTML!",
+        "class_01": "https://youtu.be/YRemMgGfbKg",
+        "class_02": "https://youtu.be/N2VlXVZJIcY",
+        "slides_01": "https://slides.com/leonnoel/html-the-basics-100devs",
+        "slides_02": "https://slides.com/leonnoel/html-more-basics-100devs",
+        "readings": {
+            "text_01":{
+                "title": "HTML & CSS: Shay Howe",
+                "link": "https://learn.shayhowe.com/html-css/"
+            }  
+        },
+        "watch": {
+            "video_01": {
+                "title": "How to study for exams - Evidence-based revision tips",
+                "link": "https://youtu.be/ukLnPbIffxE"
+            },
+            "video_02": {
+                "title": "How to Study for Exams - Spaced Repetition | Evidence-based revision tips",
+                "link": "https://youtu.be/Z-zNHHpXoMM"
+            }
+        },  
+        "do": {
+            "HW_01": "https://www.coursera.org/learn/learning-how-to-learn",
+            "HW_02": "Build: HTML for BBC Website (just what is in the attached image)"
+        }
+    },
+}
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+})
+app.get('/api/:week', (request, response) => {
+    const week = request.params.week.toLowerCase()
+    if(lessons[week]){
+        response.json(lessons[week])
+    }
+})
+
+app.listen(process.env.PORT || PORT, () =>{
+    console.log(`Server running on PORT ${PORT}`)
+})
+
+
