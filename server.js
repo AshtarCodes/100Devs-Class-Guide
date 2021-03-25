22e16995552dae7988d445a9814e16cf6bb11629
Mario De Los Santos <mario.jdls@gmail.com>
2021-03-25 17:02:42 -0400

    Added Week 02 to lessons Object

--- server.js
+++ server.js
@@ -34,6 +34,34 @@ let lessons = {
             "HW_02": "Build: HTML for BBC Website (just what is in the attached image)"
         }
     },
+    "week_02": {
+        "name": "Week_02",
+        "tagline": "Come learn the basics of CSS, and make a simple website look good!",
+        "class_01": "https://youtu.be/h3wVQJ6SNfY",
+        "class_02": "https://youtu.be/xTNCtSRz6No",
+        "slides_01": "https://slides.com/leonnoel/css-the-basics-100devs/",
+        "slides_02": "https://slides.com/leonnoel/css-more-basics-100devs/",
+        "readings": {
+            "text_01":{
+                "title": "Learn Layout",
+                "link": "http://learnlayout.com/"
+            },  
+            "text_02":{
+                "title": "Advanced HTML & CSS with Shay Howe",
+                "link": "https://learn.shayhowe.com/advanced-html-css/responsive-web-design/"
+            }  
+        },
+        "watch": {
+            "video_01": {
+                "title": "If You Want to Change the World, Start Off by Making Your Bed - William McRaven, US Navy Admiral",
+                "link": "https://youtu.be/3sK3wJAxGfs"
+            },
+        },  
+        "do": {
+            "HW_01": "https://www.typingtest.com/",
+            "HW_02": "https://www.keybr.com/"
+        }
+    },
 }
 
 app.get('/', (request, response) => {
